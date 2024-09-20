60986162f64820bb67f307d0586ac507c42fdf12
1998antonio <antoniosp1998@hotmail.com>
2024-09-20 14:07:55 -0600

    Add files via upload

--- variables.py
+++ variables.py
@@ -1,23 +1,23 @@
 #variables
 
 my_string_variable = "My string variable"
-#print(my_string_variable)
+print(my_string_variable)
 
 my_int_variable = 5
-#print(my_int_variable)
-#print(type(my_int_variable))
+print(my_int_variable)
+print(type(my_int_variable))
 
 my_float_variable = 1.8
-#print(my_float_variable)
-#print(type(my_float_variable))
+print(my_float_variable)
+print(type(my_float_variable))
 
 my_int_to_str_variable = str(my_int_variable)
-#print(my_int_to_str_variable)
-#print(type(my_int_to_str_variable))
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
 
 my_bool_variable = True
-#print(my_bool_variable)
-#print(type(my_bool_variable))
+print(my_bool_variable)
+print(type(my_bool_variable))
 
 #concatenacion de variables en un print
 print(my_string_variable,my_int_to_str_variable, my_bool_variable)
@@ -52,8 +52,8 @@ print(type(age))
 
 #Forzamos el tipo 
 address: int = 1
-#address = input('¿Cual es tu direccion? ')
-#address = True
-#address = 5
-#address = 1.2
+address = input('¿Cual es tu direccion? ')
+address = True
+address = 5
+address = 1.2
 print(type(address))
