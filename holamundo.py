6e0e69105b9cf7aa98773ce7223d5a92db68974a
1998antonio <antoniosp1998@hotmail.com>
2024-09-20 11:17:03 -0600

    Add files via upload

---
+++ holamundo.py
@@ -0,0 +1,16 @@
+"""
+UMG
+"""
+'''
+Antonio
+'''
+#print("Hola mundo")
+#print('otra vez hola mundo')
+
+#Como consultar el tipo de dato
+print(type("soy un dato str")) #tipo str
+print(type(5)) #tipo 'int'
+print(type(1.5)) #tipo 'float'
+print(type(3 + 1j)) #tipo 'complex'
+print(type(True)) #tipo 'bool'
+print(type(print("Mi cadena de texto"))) #tipo 'Nonetype
